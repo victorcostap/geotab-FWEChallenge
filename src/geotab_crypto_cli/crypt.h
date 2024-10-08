5023cd9562e37c4b45b12fb1ef0463339f28a0b2
Victor Costa <victor.costa.perez@upm.es>
2024-10-08 09:52:19 +0200

    Add documentation to the CLI program

--- src/geotab_crypto_cli/crypt.h
+++ src/geotab_crypto_cli/crypt.h
@@ -5,15 +5,48 @@
 #include <geotab_crypto.h>
 
 
+/**
+ * @brief Prints the usage information for the program.
+ *
+ * @param progName The name of the program, typically argv[0].
+ */
 void printUsage(const char *progName);
 
+/**
+ * @brief Cleans up resources by closing file pointers and freeing allocated memory.
+ *
+ * @param in Pointer to the input file to be closed.
+ * @param out Pointer to the output file to be closed.
+ * @param key Pointer to the allocated memory with the key to be freed.
+ */
 void cleanUpResources(FILE *in, FILE *out, uint8_t *key);
 
+/**
+ * @brief Retrieves the key length from the provided key file and updates the context.
+ *
+ *
+ * @param kf Pointer to the file with the stored key
+ * @param context Pointer to the cryptographic context
+ */
 void getKeyLength(FILE *kf, struct crypt_context *context);
 
+/**
+ * @brief Reads the cryptographic key from a specified file.
+ *
+ * @param keyFile String with the path of the file with the key
+ */
 void readKeyFromFile(const char *keyFile,
                             struct crypt_context *context);
 
+/**
+ * @brief Processes the input from the input stream and writes the result to the given output stream.
+ * 
+ * The operation is done character by character to allow processing the input as it arrives
+ *
+ * @param in Pointer to the input stream.
+ * @param out Pointer to the output stream.
+ * @param context Pointer to the cryptographic context with the key and other required information
+ */
 void processInput(FILE *in, FILE *out, struct crypt_context *context,
                   uint8_t *key);
 
